622be14e015d17ddf8d5a62b53bc5135dd60f138
s-chh <sachinchhabra@outlook.com>
2024-08-03 13:02:32 -0400

    Delete scripts.sh

--- scripts.sh
+++
@@ -1,4 +0,0 @@
-python main.py --dataset mnist --epochs 10
-python main.py --dataset fmnist --epochs 50
-python main.py --dataset svhn --n_channels 3 --epochs 100
-python main.py --dataset usps --epochs 50
